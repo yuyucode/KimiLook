03842f106afcd35d7b115e03515e2c899e7bb4db
yuyu <1440771511@qq.com>
2023-02-20 02:55:46 +0800

    learn: 使用 UI Toolkit 和 UI Builder 制作物品编辑器

---
+++ Assets/Editor/UI Builder/ItemEditor.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ItemEditor : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("M STUDIO/ItemEditor")]
+    public static void ShowExample()
+    {
+        ItemEditor wnd = GetWindow<ItemEditor>();
+        wnd.titleContent = new GUIContent("ItemEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        //VisualElement label = new Label("Hello World! From C#");
+        //root.Add(label);
+
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
+    }
+}
